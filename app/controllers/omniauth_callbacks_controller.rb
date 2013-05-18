97a02a7c7aeeef9ae05718f3dc3250a93bc40aa1
John Maddux <jemaddux@gmail.com>
2013-05-18 10:54:36 -0600

    bump

---
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -0,0 +1,17 @@
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def all
+    user = User.from_omniauth(request.env["omniauth.auth"])
+    user.save!
+    if user.persisted?
+      flash.notice = "Signed in!"
+      redirect_to "/"
+      # sign_in_and_redirect user
+    else
+      session["devise.user_attributes"] = user.attributes
+      redirect_to new_user_registration_url
+    end
+  end
+  alias_method :twitter, :all
+end
+
+
