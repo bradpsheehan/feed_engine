a6c99530365b1d71eaedb68f768337e52ef11d08
John Maddux <jemaddux@gmail.com>
2013-05-20 13:22:39 -0600

    oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['CONSUMER_KEY'], ENV['CONSUMER_SECRET']
+  provider :twitter, ENV['CONSUMER_KEY'], ENV['CONSUMER_SECRET'], :client_options => {:authorize_path => '/oauth/authorize'}
   provider :runkeeper, ENV['RUNKEEPER_CLIENT_ID'], ENV['RUNKEEPER_CLIENT_SECRET']
   provider :mapmyfitness, ENV['MAPMYFITNESS_CONSUMER_KEY'], ENV['MAPMYFITNESS_SECRET_KEY']
 end
