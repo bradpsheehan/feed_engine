fa5711964d895b2f753e9f412ec2732fb34eae70
Bradley Sheehan <bradpsheehan@gmail.com>
2013-05-20 15:15:06 -0600

    added auth/runkeeper url to the profile page.  Now able to integrate with runkeeper.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,7 @@ class RegistrationsController < ApplicationController
 
   def create
     Registrar.register(auth: env['omniauth.auth'], user: current_user)
-    redirect_to dashboard_path, notice: "Successfully registered."
+    redirect_to new_run_path, notice: "Successfully registered."
   end
 
 end
