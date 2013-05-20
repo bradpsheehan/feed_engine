c7e95d543dfda621b4933653c11d406617f91f6a
lalalainexd <lalalainexd@gmail.com>
2013-05-20 14:21:34 -0600

    added optional size parameter to user.photo

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,8 +20,8 @@ class User < ActiveRecord::Base
     @friends_tweets = @friends_tweets.flatten
   end
 
-  def photo
-    @photo ||= twitter.user(name).profile_image_url(:original)
+  def photo(size=:normal)
+    @photo ||= twitter.user(name).profile_image_url(size)
     #profile_image_url(:normal)
   end
 
