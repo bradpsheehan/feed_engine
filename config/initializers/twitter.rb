9ebc16317540d28189c49a094f8f0c301f3aff06
John Maddux <jemaddux@gmail.com>
2013-05-20 11:58:24 -0600

    merged

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,6 +1,6 @@
 Twitter.configure do |config|
-  config.consumer_key = "SQXPKncl9wOlvvKwf1T5w"
-  config.consumer_secret = "tDwWFZ3pkvhWEf5VXDN0Obgfwhu1G0uJLoXK6ogmbs"
+  config.consumer_key = "ukOeH0KtECU7dvzvuMx9OA"
+  config.consumer_secret = "0633qRT9loRaJWToCvYtrzh5tfQHHWztUBl9KXqo"
   config.oauth_token = "61654242-W6Ck8S0KcarCPkZPv0OdVIhiMaRDgByDMweN7Sqmg"
   config.oauth_token_secret = "tL8MZx8X6Kvmxj2BPCwBQ3EvgWDX0ccXHHj4YnSM7ew"
 end
