373e74bd12012fca27f0c85e6673a76200f80816
John Maddux <jemaddux@gmail.com>
2013-05-20 13:07:28 -0600

    changed env variables

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,6 +1,6 @@
 Twitter.configure do |config|
-  config.consumer_key = "ukOeH0KtECU7dvzvuMx9OA"
-  config.consumer_secret = "0633qRT9loRaJWToCvYtrzh5tfQHHWztUBl9KXqo"
+  config.consumer_key = ENV['CONSUMER_KEY']
+  config.consumer_secret = ENV['CONSUMER_SECRET']
   config.oauth_token = "61654242-W6Ck8S0KcarCPkZPv0OdVIhiMaRDgByDMweN7Sqmg"
   config.oauth_token_secret = "tL8MZx8X6Kvmxj2BPCwBQ3EvgWDX0ccXHHj4YnSM7ew"
 end
