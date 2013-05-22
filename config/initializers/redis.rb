2ce30dad4407e81143229d5e4db4612cffa1fa0f
John Maddux <jemaddux@gmail.com>
2013-05-22 10:05:48 -0600

    edited redistogo url

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -3,7 +3,8 @@ if Rails.env.development?
   $redis = Redis.new(:host => "localhost", :port => 6379)
 else
   # on Heroku, use the url that is preset
-  $redis = Redis.connect :url => ENV["OPENREDIS_URL"]
+  ENV["REDISTOGO_URL"] ||= "redis://redistogo:f2659d6e672e806f7e8d097f3279a330@viperfish.redistogo.com:9637/"
+  $redis = Redis.connect :url => ENV["REDISTOGO_URL"]
   # you must set the Resque.redis to point to this redis explicitly for some reason
   Resque.redis = $redis
 end
